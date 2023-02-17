8980ce2c7c9fc35f11f03e10e771bcf20e4232c5
ImperatorOfIntellectualism <83208504+ImperatorOfIntellectualism@users.noreply.github.com>
2023-02-17 13:53:23 +0700

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,8 @@
 import mysql from "mysql"
 
 export const db = mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:'15042001sd',
-    database:'lod'
+    host:"***",
+    user:"***",
+    password:'***',
+    database:'***'
 })
